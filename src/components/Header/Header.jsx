d7f11b3b4d721b1dad81595ffa1a7265cac28937
Roman <romandybinetc@gmail.com>
2020-06-10 19:39:26 +0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from'./Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGS7XHiviU5kGzbLpRi5hO8CqZTmXa9gOameP8mz1lPm0Am1DT4w&s' />
+        </header>
+    )
+}
+
+
+
+export default Header;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './Profile.module.css';
+
+const Profile = () => {
+    return (
+        <div className={styles.content}>
+            <div>
+                <img src='https://www.coalindia.in/new_web/images/bg2_tmp.jpg' />
+            </div>
+            <div>
+                ava+description
+        </div>
+            <div>
+                My Posts
+          <div>
+                    New Post
+          </div>
+                <div className={styles.posts}>
+                    <div className={styles.item}>
+                        Post 1
+             </div>
+                    <div className={styles.item}>
+                        Post 2
+             </div>
+                    <div className={styles.item} >
+                        Post 3
+             </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Profile;
