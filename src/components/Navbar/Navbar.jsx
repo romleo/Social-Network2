428bda55fc72a21276affae13e48cc341f03d9e1
Roman <romandybinetc@gmail.com>
2020-06-12 18:07:59 +0300

    update routings2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import styles from './Navbar.module.css';
-
+import {NavLink} from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className={styles.nav}>
            
             <div className={styles.item}>
-                <a href="/profile">Profile</a>
+                <NavLink to="/profile">Profile</NavLink> 
             </div>
             <div className={`${styles.item} ${styles.active}`}>
-                <a href="dialogs">Massages</a>
+            <NavLink to="/dialogs">Messages</NavLink> 
             </div>
             <div className={styles.item}>
                 <a>News</a>
