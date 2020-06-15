237b9d508c4ee16a7cdce61a6f34b692c34ecb34
Roman <romandybinetc@gmail.com>
2020-06-15 13:36:46 +0300

    add dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,30 @@ import styles from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div >
-            Dialogs
+        <div className="dialogs">
+            <div className="dialogs-items">
+                <div className='dialog '>
+                    Bogdan
+                </div>
+                <div className='dialog '>
+                    Roman
+                </div>
+                <div className='dialog'>
+                    Volodymyr
+                </div>
+                <div className='dialog'>
+                    Oleksiy
+                </div>
+                <div className='dialog'>
+                    Yriy
+                </div>
+            </div>
+            <div className="messages">
+                <div className="message">Hi</div>
+                <div className="message">Hos are you?</div>
+                <div className="message">HELLO FRIEND</div>
+                <div className="message">YO!</div>
+            </div>
         </div>
     )
 }
