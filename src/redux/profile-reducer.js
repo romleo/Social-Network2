f0b958b025dff5ace14f6fe11775f3c70c498487
Roman <romandybinetc@gmail.com>
2020-12-14 18:03:11 +0200

    changed reduser

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -15,18 +15,24 @@ let initialState = {
 };
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST:
+        case ADD_POST:{
             let newPost = {
                 id: 4,
                 massage: state.newPostTrext,
                 likesCount: 0
             };
-            state.posts.push(newPost);
-            state.newPostTrext = '';
-            return state;
-        case UPDATE_NEW_POST_TEXT:
-            state.newPostTrext = action.newText;
-            return state;
+            let stateCopy = {...state};
+            stateCopy.posts = [...state.posts];
+            stateCopy.posts.push(newPost);
+            stateCopy.newPostTrext = '';
+            return stateCopy;
+        }
+        case UPDATE_NEW_POST_TEXT:{
+            let stateCopy = {...state}
+            stateCopy.posts = [...state.posts];
+            stateCopy.newPostTrext = action.newText;
+            return stateCopy;
+        }
         default:
             return state;
     }
